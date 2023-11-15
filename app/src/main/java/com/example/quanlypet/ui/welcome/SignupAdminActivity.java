5dcf2bdb2ef246603297c2e534997155f4fdaa22
Thanhdevv <thanhhcph32668@fpt.edu.vn>
2023-11-15 19:12:47 +0700

    update

---
+++ app/src/main/java/com/example/quanlypet/ui/welcome/SignupAdminActivity.java
@@ -0,0 +1,15 @@
+package com.example.quanlypet.ui.welcome;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.quanlypet.R;
+public class SignupAdminActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_admin);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/quanlypet/ui/welcome/SignupUsersActivity.java
@@ -0,0 +1,16 @@
+package com.example.quanlypet.ui.welcome;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.quanlypet.R;
+public class SignupUsersActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_users);
+
+
+    }
+}
